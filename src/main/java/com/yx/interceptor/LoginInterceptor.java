c2ab04fbeb1741be660f4ed8cf967b67d920ef6a
Wen Yin <andywenyin@gmail.com>
2024-03-10 20:43:18 -0400

    Update LoginInterceptor.java

--- src/main/java/com/yx/interceptor/LoginInterceptor.java
+++ src/main/java/com/yx/interceptor/LoginInterceptor.java
@@ -1,5 +1,25 @@
 package com.yx.interceptor;
 
-public class LoginInterceptor {
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("Executed after handling the request and before sending the response....");
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        //Intercepter pendant le traitement
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
 }
