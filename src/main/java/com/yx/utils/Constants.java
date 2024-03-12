15374c351d9774066c9194470ac56e46a376b4f9
Wen Yin <andywenyin@gmail.com>
2024-03-12 02:25:39 -0400

    Create Constants.java

---
+++ src/main/java/com/yx/utils/Constants.java
@@ -0,0 +1,18 @@
+package com.yx.utils;
+
+public class Constants {
+
+    public final static int OK_CODE = 0;    //0 means OK
+
+    public final static int FAIL_CODE = 400;
+
+    public final static String OK_MSG = "Request successful";
+
+    public final static String FAIL_MSG = "Request failed";
+
+    public final static int STATUS_0 = 0;//Available status
+
+    public final static int STATUS_1 = 1;//Disabled status
+
+    public final static String CACHE_NAME = "KACache";
+}
